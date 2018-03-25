155949c978d453ea0c15822d2e87b50543fbaea0
Glitch (endurable-sky) <none>
2018-03-25 22:20:56 +0000

    💇🐊 Fix ip address issue

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 // server.js
 // where your node app starts
-
+'use strict';
 // init project
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express')
 const useragent = require('express-useragent');
 const app = express()
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
 app.use(bodyParser.json());
 app.use(cors());
 app.use(useragent.express());
+// This ensures the correct IP address is outputted:
+// https://stackoverflow.com/questions/10849687/express-js-how-to-get-remote-client-address
+// Haozhun's solution solved my issue
+app.enable('trust proxy');
 
 // API URL
 const api = '/api/whoami';
 
-app.get(api, (req, res, next) => {
+app.get(api, (req, res) => {
   const ipaddress = req.ip;
   const language = req.acceptsLanguages();
   const software = `OS: ${req.useragent.os}; Browser: ${req.useragent.browser}`;
